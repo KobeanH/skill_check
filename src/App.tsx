cab207e83fed32f13aca660d7efb479c23bf9c42
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-09-18 15:47:38 +0900

    環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [post, setPost] = React.useState(null)
+
+  React.useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/').then((response) => {
+      setPost(response.data)
+    })
+  }, [])
+  console.log(post)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <p>{post}foakfo</p> */}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
